6edd91c9992ab1a32ed7979fdbcc7ca4b41c2b96
Rafael Silva <rafael.silva@aimopark.se>
2024-10-02 11:31:04 +0200

    First commit

---
+++ tests/testsuite.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Front-end tests', () => {
+  test('Create a client', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+    await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+    // Include the rest of the code...
+
+  });
+});
+
+test.describe('Backend tests', () => {
+  test('Create a client', async ({ request }) => {
+    const response = await request.post('http://localhost:3000/api/login', {
+      data:{
+        "username": `${process.env.TEST_USERNAME}`,
+        "password": `${process.env.TEST_PASSWORD}`
+      }      
+    });
+    expect (response.ok()).toBeTruthy();    
+    // Include the rest of the code...
+  });  
+});
+
